f0e71f49e365b97bf6fdfb330f05c23d570a9fd7
SSingh883710 <ssingh883710@gmail.com>
2021-09-01 17:13:32 +0530

    mysql file uncommented.

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,20 +1,20 @@
-// const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
-// const sequelize = new Sequelize(process.env.DATABASE,
-//     process.env.USERNAME,
-//     process.env.PASSWORD, {
-//         host: process.env.HOST,
-//         dialect: 'mysql',
-//     });
-// sequelize
-//     .authenticate()
-//     .then(() => {
-//         console.log('Connection has been established successfully.');
-//     })
-//     .catch(err => {
-//         console.error('Unable to connect to the database:', err);
-//     });
+const sequelize = new Sequelize(process.env.DATABASE,
+    process.env.USERNAME,
+    process.env.PASSWORD, {
+        host: process.env.HOST,
+        dialect: 'mysql',
+    });
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
-// module.exports = {
-//     sequelize,
-// };
+module.exports = {
+    sequelize,
+};
